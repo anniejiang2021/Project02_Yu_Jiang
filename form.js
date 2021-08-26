4bf421ca47b01a0438c4aae5d2ecb1c3066da0cf
Yu Jiang <anniejiang2008@hotmail.com>
2021-08-26 15:22:38 +1000

    with cat help, i complete the project 2

--- form.js
+++ form.js
@@ -1,6 +1,3 @@
-
-
-/*function validation() {alert("working");}*/
 const lastname = document.querySelector("#last-name");
 const firstname = document.querySelector("#first-name");
 const email = document.querySelector("#email");
@@ -10,7 +7,8 @@ const form = document.querySelector(".contact-form");
 const closeModal = document.querySelector("#close-modal")
 const modal = document.querySelector(".modal")
 const overlay = document.querySelector(".overlay")
-const body = document.querySelector("body")
+ const body = document.querySelector(".modal-active")
+const hidden=document.querySelector(".hidden")
 
 
 const validLastname = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 \.'\-]+$/
@@ -20,36 +18,46 @@ const validTelephone = /^[0-9]+$/;
 
 
 form.setAttribute("novalidate",true)
-form.onsumbit = submitForm   /*onsubmit="return vslidstion(all items)"????*/
+form.onsubmit = submitForm  
 
  
 function submitForm(event){
     console.log("hello")
-    event.preventDefault()}  //prevent the form from autosubmitting//
+    event.preventDefault()  //prevent the form from autosubmitting//
+
+    
 
 
-if (inputValid(validLastname,lastname)&&inputValid(validEmail,email)/*&&inputValid(validFirstname,firstname)
-&&isEmpty(message.value)&&inputValid(validTelephone,telephone))*/) {
-    console.log('Lastname: ${lastname.value}\n Email: ${email.value}')
-    console.log(model);
-    model.style.display="block"
-}
-console.log('Input are incorrect, try again.')
+if (inputValid(validLastname,lastname)&&inputValid(validEmail,email)&&inputValid(validFirstname,firstname)
+&&isEmpty(message.value)&&inputValid(validTelephone,telephone)) {
+    console.log(`Lastname: ${lastname.value}\n Email: ${email.value}\n Firstname:${firstname.value}\n 
+    Telephone:${telephone.value}\n Message:${message.value}`)
+    console.log(modal);
+     modal.style.display="block";
 
-   /* inputValid(validName,name)
-    inputValid(validEmail,email)
-    function isEmpty(input) {return input.trim().length;}*/
+openModal()
+      
+    
+
+} else{
+console.log('Input are incorrect, try again.')}}
+
+ function isEmpty(input) {return input.trim().length;}
 
 function inputValid(regex,input) {
     return regex.test(input.value)
 }
 
+
+
 function openModal(){
-    body.classlist.remove("modal-aactive")
-    overlay.classlist.add("hidden")
-    openModal.classlist.add("hidden")
-}
-/*closeModalButton.onclick =closeModal
+   document.querySelector(".overlay").style.display = "block";
+     
+ }
+    
+    
+
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
-})*/
+    overlay.style.display = 'none'; document.getElementById("contact-form").reset();
+})
